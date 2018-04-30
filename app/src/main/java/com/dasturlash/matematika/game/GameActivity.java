1a7bcd88e4b7de06d1f4311ce5f0b0c6b4b5d5cc
QAREKEN <kalabaevshj@gmail.com>
2018-04-30 18:51:23 +0500

    Variantlar basiladi keyingi Taskqa otedi

--- app/src/main/java/com/dasturlash/matematika/game/GameActivity.java
+++ app/src/main/java/com/dasturlash/matematika/game/GameActivity.java
@@ -3,6 +3,8 @@ package com.dasturlash.matematika.game;
 import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
@@ -20,7 +22,8 @@ public class GameActivity extends AppCompatActivity implements VariantListener {
     private Button variantB;
     private Button variantC;
     private Button variantD;
-    private int correctVariant;
+    private Integer correctVariant;
+    private int type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +33,19 @@ public class GameActivity extends AppCompatActivity implements VariantListener {
         variantB = findViewById(R.id.variantB);
         variantC = findViewById(R.id.variantC);
         variantD = findViewById(R.id.variantD);
+        variantA.setOnClickListener(onClickListener);
+        variantB.setOnClickListener(onClickListener);
+        variantC.setOnClickListener(onClickListener);
+        variantD.setOnClickListener(onClickListener);
+        type = getIntent().getIntExtra(MainActivity.EXTRA_BUTTON_TYPE, 0);
         linearLayout = findViewById(R.id.customViewContainer);
-        nextTask(getIntent().getIntExtra(MainActivity.EXTRA_BUTTON_TYPE, 0));
+        nextTask(type);
     }
 
     private void nextTask(int type) {
         if (customView != null ) {
-            customView = null;
+            customView.clearAnimation();
+            ((ViewManager) customView.getParent()).removeView(customView);
         }
         customView = new CustomView(GameActivity.this, type, null);
         variantMaker = new VariantMaker(customView.getFirstNumber(), customView.getSecondNumber(), customView.getType(), this);
@@ -47,7 +56,7 @@ public class GameActivity extends AppCompatActivity implements VariantListener {
 
     private void startAnimation(CustomView view) {
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.slide_down);
-        animation.setDuration(5000);
+        animation.setDuration(20000);
         view.startAnimation(animation);
     }
 
@@ -58,5 +67,17 @@ public class GameActivity extends AppCompatActivity implements VariantListener {
         variantB.setText(variants[1].toString());
         variantC.setText(variants[2].toString());
         variantD.setText(variants[3].toString());
+        correctVariant = variantMaker.getCorrectVariant();
     }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (((Button) view).getText() == correctVariant.toString()) {
+                nextTask(type);
+            } else {
+                return;
+            }
+        }
+    };
 }
